989711b76ebfe3b0efc3684b08f148d5104e98f4
erhanask <erhanasik8@gmail.com>
2022-04-22 13:55:14 +0300

    edits and fixes for my ads page.

--- src/Http/Controller/AjaxController.php
+++ src/Http/Controller/AjaxController.php
@@ -4,6 +4,7 @@ use Anomaly\Streams\Platform\Http\Controller\PublicController;
 use Anomaly\Streams\Platform\Support\Currency;
 use Anomaly\UsersModule\User\UserModel;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 use Visiosoft\AdvsModule\Adv\AdvModel;
 use Illuminate\Http\Request;
 use Visiosoft\AdvsModule\Adv\Contract\AdvRepositoryInterface;
@@ -60,22 +61,50 @@ class AjaxController extends PublicController
     {
         $my_advs = new AdvModel();
         $type = $request->type;
-        if ($type === 'pending') {
-            $page_title = trans('visiosoft.module.advs::field.pending_adv.name');
-            $my_advs = $my_advs->pendingAdvsByUser();
+
+        if (!$request->simple) {
+            if ($type === 'pending') {
+                $page_title = trans('visiosoft.module.advs::field.pending_adv.name');
+                $my_advs = $my_advs->pendingAdvsByUser();
+            } else {
+                $page_title = trans('visiosoft.module.advs::field.my_adv.name');
+                $my_advs = $my_advs->myAdvsByUser();
+            }
+            $my_advs = $my_advs
+                ->select(['id', 'cover_photo', 'slug', 'price', 'currency', 'city', 'country_id', 'cat1', 'cat2', 'status', 'created_at'])
+                ->where(function ($q) {
+                    if ($this->request->search) {
+                        return $q->where('id', 'LIKE', '%' . $this->request->search . '%');
+                    }
+                    return $q;
+                });
         } else {
-            $page_title = trans('visiosoft.module.advs::field.my_adv.name');
-            $my_advs = $my_advs->myAdvsByUser();
+            $my_advs = $my_advs
+                ->userAdv()
+                ->select(['id', 'cover_photo', 'slug', 'price', 'currency', 'city', 'country_id', 'cat1', 'cat2', 'status', 'created_at']);
+
+            if ($type == 'approved') {
+                $page_title = trans('visiosoft.module.advs::field.my_adv.name');
+                $my_advs = $my_advs->where('status','=','approved');
+            } else if ($type == 'passive') {
+                $page_title = trans('visiosoft.module.advs::field.passive_adv.name');
+                $my_advs = $my_advs->where('status','=','passive');
+            } else if ($type == 'pending') {
+                $page_title = trans('visiosoft.module.advs::field.pending_adv.name');
+                $my_advs = $my_advs->where('status','=','pending_user')
+                                   ->orWhere('status','=','pending_admin');
+            } else if ($type == 'incomplete') {
+                $page_title = trans('theme::field.incomplete_ads');
+                $my_advs = new AdvModel();
+                $my_advs = $my_advs
+                    ->select(['id', 'cover_photo', 'slug', 'price', 'currency', 'city', 'country_id', 'cat1', 'cat2', 'status', 'created_at'])
+                    ->where('slug','=','')
+                    ->where('advs_advs.created_by_id', Auth::id());
+            }
         }
-        $my_advs = $my_advs
-            ->select(['id', 'cover_photo', 'slug', 'price', 'currency', 'city', 'country_id', 'cat1', 'cat2', 'status', 'created_at'])
-            ->where(function($q) {
-                if ($this->request->search) {
-                    return $q->where('id', 'LIKE', '%' . $this->request->search . '%');
-                }
-                return $q;
-            })
-            ->orderByDesc('id');
+
+        $my_advs = $my_advs->orderByDesc('id');
+
 
         if (\request()->paginate === 'true') {
             $my_advs = $advRepository->addAttributes($my_advs->paginate(setting_value('streams::per_page')));
