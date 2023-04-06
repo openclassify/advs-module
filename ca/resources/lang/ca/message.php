b3e6797b9f880ec12e7cbf85f70c5a234840cb43
Fatih Alp <fatihalp89@gmail.com>
2023-04-06 09:48:21 +0300

    New translations message.php (Catalan)

--- ca/resources/lang/ca/message.php
+++ ca/resources/lang/ca/message.php
@@ -40,5 +40,6 @@ return [
     'you_can_not_delete_a_system_status' => 'You can not delete a system status!',
     'product_added_to_cart' => 'Product added to cart!',
 
-    'error_select_related_category' => 'Please select a related category!'
+    'error_select_related_category' => 'Please select a related category!',
+    'error_select_ad' => 'You have not selected any ads. Please select one or more ads!'
 ];
