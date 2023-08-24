d82e3284bb8e6ff927b6a138316b0ecad024e307
profstyle1 <46463523+profstyle1@users.noreply.github.com>
2023-08-24 13:01:46 +0300

    New translations stream.php (Swedish)

--- sv-SE/resources/lang/sv/stream.php
+++ sv-SE/resources/lang/sv/stream.php
@@ -8,7 +8,7 @@ return [
         'name' => 'Kategorier',
     ],
     'village' => [
-        'name' => 'By',
+        'name' => 'Ort',
     ],
     'custom_fields' => [
         'name' => 'Anpassade fält',
