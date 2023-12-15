86302b1f3b8c618e1de79341035ecd511eeec849
emrullahardc <emrullahardc@gmail.com>
2023-12-15 17:52:45 +0300

    TranslateFixer added.

--- src/Adv/AdvObserver.php
+++ src/Adv/AdvObserver.php
@@ -9,9 +9,43 @@ use Visiosoft\AdvsModule\Adv\Event\DeletingAd;
 
 class AdvObserver extends EntryObserver
 {
+    private function translateFixer($entryId)
+    {
+        $defaultLocale = setting_value('streams::default_locale');
+        $translations = DB::table('advs_advs_translations')
+            ->select(['locale', 'name'])
+            ->where('entry_id', $entryId)
+            ->whereNotNull('locale')
+            ->get();
+
+        if ($translations) {
+            $name = "";
+            foreach ($translations as $translation) {
+                if ($translation->locale == $defaultLocale && !empty($translation->name)) {
+                    $name = $translation->name;
+                    break;
+                } else if (!empty($translation->name)) {
+                    $name = $translation->name;
+                }
+            }
+
+            DB::table('advs_advs_translations')
+                ->whereNull('name')
+                ->where('entry_id', $entryId)
+                ->update(['name' => $name]);
+        }
+
+    }
+
+    public function created(EntryInterface $entry)
+    {
+        $this->translateFixer($entry->getId());
+    }
+
     public function updating(EntryInterface $entry)
     {
         $this->dispatchSync(new AddSlug($entry));
+        $this->translateFixer($entry->getId());
 
         parent::updating($entry);
     }
